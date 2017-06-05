ead87f7d186644860fe70ce66e1e07bd440f9aa2
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-06-05 19:04:46 +0300

    rm everything

--- lib/bencview/meta.rb
+++
@@ -1,9 +0,0 @@
-module Bencview
-  module Meta
-    NAME = 'bencview'
-    VERSION = '0.0.3'
-    AUTHOR = 'Alexander Gromnitsky'
-    EMAIL = 'alexander.gromnitsky@gmail.com'
-    HOMEPAGE = 'http://github.com/gromnitsky/' + NAME
-  end
-end
--- test/helper.rb
+++
@@ -1,4 +0,0 @@
-# This is supposed to be your helper for all your test. Feel free to
-# add staff here.
-
-require_relative 'helper_trestle'
